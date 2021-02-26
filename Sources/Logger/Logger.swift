5651640699293dc56274f207a232f20e6c45ab68
Andrius Shiaulis <shiaulis@gmail.com>
2021-02-26 23:06:43 +0200

    Update logger code

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,3 +1,68 @@
-struct Logger {
-    var text = "Hello, World!"
+//
+//  Logger.swift
+//  EstonianWeather
+//
+//  Created by Andrius Shiaulis on 28.09.2020.
+//
+
+import Foundation
+
+public enum LoggerModule {
+    case dataParser, mainViewModel, dataMapper, ratingService, purchases
+
+    fileprivate var name: String {
+        switch self {
+        case .dataParser: return "🔎 Data Parser"
+        case .mainViewModel: return "🧑‍🔧 Main View Model"
+        case .dataMapper: return "💿 Data Mapper"
+        case .ratingService: return "✨ Rating Service"
+        case .purchases: return "💰 Purchases"
+        }
+    }
+}
+
+public protocol Logger {
+
+    func log(message: String, error: Error, module: LoggerModule)
+    func logNotImplemented(functionality: String, module: LoggerModule)
+    func log(information: String, module: LoggerModule)
+    func log(successState: String, module: LoggerModule)
+
+}
+
+public final class PrintLogger {
+
+    public init() {}
+
+    private var currentDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        dateFormatter.locale = .current
+        return dateFormatter.string(from: Date())
+    }
+
+    private func log(_ string: String) {
+        print("\(self.currentDate): \(string)")
+    }
+}
+
+extension PrintLogger: Logger {
+
+    public func log(message: String, error: Error, module: LoggerModule) {
+        log("❌ \(module.name): \(message) Error: \(error.localizedDescription)")
+        assertionFailure()
+    }
+
+    public func logNotImplemented(functionality: String, module: LoggerModule) {
+        log("🚧 \(module.name): \(functionality) not implemented yet")
+    }
+
+    public func log(information: String, module: LoggerModule) {
+        log("ℹ️ \(module.name): \(information)")
+    }
+
+    public func log(successState: String, module: LoggerModule) {
+        log("✅ \(module.name): \(successState)")
+    }
+
 }
--- Tests/LoggerTests/LoggerTests.swift
+++ Tests/LoggerTests/LoggerTests.swift
@@ -6,7 +6,7 @@ final class LoggerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Logger().text, "Hello, World!")
+        XCTAssertNotNil(PrintLogger())
     }
 
     static var allTests = [
